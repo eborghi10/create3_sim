ac89c498201c45ea5558ba8ab80812343ede4a39
LolaSegura <lsegura@ekumenlabs.com>
2021-10-15 17:31:37 -0300

    Publish mock messages for topics not being used in the sim

---
+++ irobot_create_toolbox/include/irobot_create_toolbox/mock_publisher.hpp
@@ -0,0 +1,105 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Lola Segura (lsegura@irobot.com)
+
+#pragma once
+
+#include <sensor_msgs/msg/battery_state.hpp>
+#include <irobot_create_msgs/msg/button.hpp>
+#include <irobot_create_msgs/msg/hazard_detection.hpp>
+#include <irobot_create_msgs/msg/hazard_detection_vector.hpp>
+#include <irobot_create_msgs/msg/interface_buttons.hpp>
+#include <irobot_create_msgs/msg/kidnap_status.hpp>
+#include <irobot_create_msgs/msg/led_color.hpp>
+#include <irobot_create_msgs/msg/lightring_leds.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <irobot_create_msgs/msg/slip_status.hpp>
+#include <irobot_create_msgs/msg/stop_status.hpp>
+#include <irobot_create_msgs/msg/wheel_vels.hpp>
+#include <irobot_create_toolbox/parameter_helper.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <string>
+
+namespace irobot_create_toolbox
+{
+class MockPublisher : public rclcpp::Node
+{
+public:
+  /// \brief Constructor
+  MockPublisher();
+
+  // Callback functions
+  void kidnap_callback(irobot_create_msgs::msg::HazardDetectionVector::SharedPtr msg);
+  void stop_callback(nav_msgs::msg::Odometry::SharedPtr msg);
+  void lightring_callback(irobot_create_msgs::msg::LightringLeds::SharedPtr msg);
+
+protected:
+  // Publish aggregated detections on timer_'s frequency
+  rclcpp::TimerBase::SharedPtr buttons_timer_;
+  rclcpp::TimerBase::SharedPtr slip_status_timer_;
+  rclcpp::TimerBase::SharedPtr kidnap_status_timer_;
+  rclcpp::TimerBase::SharedPtr battery_state_timer_;
+  rclcpp::TimerBase::SharedPtr stop_status_timer_;
+
+  // Publishers
+  std::shared_ptr<rclcpp::Publisher<irobot_create_msgs::msg::InterfaceButtons>> buttons_publisher_;
+  rclcpp::Publisher<irobot_create_msgs::msg::SlipStatus>::SharedPtr slip_status_publisher_;
+  rclcpp::Publisher<irobot_create_msgs::msg::KidnapStatus>::SharedPtr kidnap_status_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::BatteryState>::SharedPtr battery_state_publisher_;
+  rclcpp::Publisher<irobot_create_msgs::msg::StopStatus>::SharedPtr stop_status_publisher_;
+
+  // Subscribers
+  rclcpp::Subscription<irobot_create_msgs::msg::HazardDetectionVector>::SharedPtr kidnap_status_subscription_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr stop_status_subscription_;
+  rclcpp::Subscription<irobot_create_msgs::msg::LightringLeds>::SharedPtr lightring_subscription_;
+
+
+  // Topic to publish interface buttons to
+  std::string buttons_publisher_topic_;
+  // Topic to publish slip status to
+  std::string slip_status_publisher_topic_;
+  // Topic to publish kidnap status to
+  std::string kidnap_status_publisher_topic_;
+  // Topic to publish battery state to
+  std::string battery_state_publisher_topic_;
+   // Topic to publish stop status to
+  std::string stop_status_publisher_topic_;
+
+  // Topic to subscribe to hazard detection vector
+  std::string hazard_subscription_topic;
+  // Topic to subscribe to wheel vels vector
+  std::string wheel_vels_subscription_topic;
+  // Topic to subscribe to light ring vector
+  std::string lightring_subscription_topic;
+
+  // Message to store the interface buttons
+  irobot_create_msgs::msg::InterfaceButtons buttons_msg_;
+  // Message to store the slip status
+  irobot_create_msgs::msg::SlipStatus slip_status_msg_;
+  // Message to store the kidnap status
+  irobot_create_msgs::msg::KidnapStatus kidnap_status_msg_;
+  // Message to store the battery state
+  sensor_msgs::msg::BatteryState battery_state_msg_;
+  // Message to store the stop status
+  irobot_create_msgs::msg::StopStatus stop_status_msg_;
+
+  bool kidnap_status_ = false;
+  bool stop_status_ = true;
+
+  float pose_x = 0, pose_y = 0, pose_z = 0;
+  float orientation_x = 0, orientation_y = 0, orientation_z = 0, orientation_w = 0;
+};
+
+}  // namespace irobot_create_toolbox
---
+++ irobot_create_toolbox/src/mock_publisher.cpp
@@ -0,0 +1,183 @@
+// Copyright 2021 iRobot, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// @author Lola Segura (lsegura@irobot.com)
+
+#include <irobot_create_toolbox/mock_publisher.hpp>
+
+namespace irobot_create_toolbox
+{
+MockPublisher::MockPublisher()
+: rclcpp::Node("mock_publisher_node")
+{
+  // Topic parameter to publish buttons to
+  buttons_publisher_topic_ = declare_and_get_parameter<std::string>("button_topic", this);
+  // Topic parameter to publish slip status to
+  slip_status_publisher_topic_ = declare_and_get_parameter<std::string>("slip_status_topic", this);
+  // Topic parameter to publish kidnap status to
+  kidnap_status_publisher_topic_ = declare_and_get_parameter<std::string>("kidnap_status_topic", this);
+  // Topic parameter to publish battery state to
+  battery_state_publisher_topic_ = declare_and_get_parameter<std::string>("battery_state_topic", this);
+  // Topic parameter to publish stop status to
+  stop_status_publisher_topic_ = declare_and_get_parameter<std::string>("stop_status_topic", this);
+
+  // Subscriber topics
+  hazard_subscription_topic = declare_and_get_parameter<std::string>("hazard_topic", this);
+  wheel_vels_subscription_topic = declare_and_get_parameter<std::string>("wheel_vels_topic", this);
+  lightring_subscription_topic = declare_and_get_parameter<std::string>("lightring_topic", this);
+
+  // Publish rate parameters
+  const double buttons_publish_rate = declare_and_get_parameter<double>("buttons_publish_rate", this);  // Hz
+  const double slip_status_publish_rate = declare_and_get_parameter<double>("slip_status_publish_rate", this);  // Hz
+  const double kidnap_status_publish_rate = declare_and_get_parameter<double>("kidnap_status_publish_rate", this);  // Hz
+  const double battery_state_publish_rate = declare_and_get_parameter<double>("battery_state_publish_rate", this);  // Hz
+
+  // Define buttons publisher
+  buttons_publisher_ = create_publisher<irobot_create_msgs::msg::InterfaceButtons>(
+    buttons_publisher_topic_, rclcpp::SensorDataQoS());
+  RCLCPP_INFO_STREAM(get_logger(), "Advertised topic: " << buttons_publisher_topic_);
+
+  // Define slip status publisher
+  slip_status_publisher_ = create_publisher<irobot_create_msgs::msg::SlipStatus>(
+    slip_status_publisher_topic_, rclcpp::SensorDataQoS());
+  RCLCPP_INFO_STREAM(get_logger(), "Advertised topic: " << slip_status_publisher_topic_);
+
+  // Define kidnap status publisher
+  kidnap_status_publisher_ = create_publisher<irobot_create_msgs::msg::KidnapStatus>(
+    kidnap_status_publisher_topic_, rclcpp::SensorDataQoS());
+  RCLCPP_INFO_STREAM(get_logger(), "Advertised topic: " << kidnap_status_publisher_topic_);
+
+  // Define battery state publisher
+  battery_state_publisher_ = create_publisher<sensor_msgs::msg::BatteryState>(
+    battery_state_publisher_topic_, rclcpp::SensorDataQoS());
+  RCLCPP_INFO_STREAM(get_logger(), "Advertised topic: " << battery_state_publisher_topic_);
+
+  // Define stop status publisher
+  stop_status_publisher_ = create_publisher<irobot_create_msgs::msg::StopStatus>(
+    stop_status_publisher_topic_, rclcpp::SensorDataQoS());
+  RCLCPP_INFO_STREAM(get_logger(), "Advertised topic: " << stop_status_publisher_topic_);
+
+ // Subscription to the hazard detection vector
+  kidnap_status_subscription_ = create_subscription<irobot_create_msgs::msg::HazardDetectionVector>(
+    hazard_subscription_topic, rclcpp::SensorDataQoS(), std::bind(&MockPublisher::kidnap_callback, this, std::placeholders::_1));
+  RCLCPP_INFO_STREAM(get_logger(), "Subscription to topic: " << hazard_subscription_topic);
+  // Subscription to the stop status
+  stop_status_subscription_ = create_subscription<nav_msgs::msg::Odometry>(
+    wheel_vels_subscription_topic, rclcpp::SensorDataQoS(), std::bind(&MockPublisher::stop_callback, this, std::placeholders::_1));
+  RCLCPP_INFO_STREAM(get_logger(), "Subscription to topic: " << wheel_vels_subscription_topic);
+
+   // Subscription to the lightring leds
+  lightring_subscription_ = create_subscription<irobot_create_msgs::msg::LightringLeds>(
+    lightring_subscription_topic, rclcpp::SensorDataQoS(), std::bind(&MockPublisher::lightring_callback, this, std::placeholders::_1));
+  RCLCPP_INFO_STREAM(get_logger(), "Subscription to topic: " << lightring_subscription_topic);
+
+  buttons_timer_ = create_wall_timer(std::chrono::duration<double>(1 / buttons_publish_rate), [this]() {
+    // Set header timestamp.
+    this->buttons_msg_.header.stamp = now();
+
+    this->buttons_msg_.button_1.header.stamp = now();
+    this->buttons_msg_.button_power.header.stamp = now();
+    this->buttons_msg_.button_2.header.stamp = now();
+
+    // Publish topics
+    this->buttons_publisher_->publish(this->buttons_msg_);
+  });
+
+  slip_status_timer_ = create_wall_timer(std::chrono::duration<double>(1 / slip_status_publish_rate), [this]() {
+    // Set header timestamp.
+    this->slip_status_msg_.header.stamp = now();
+
+    // Publish topics
+    this->slip_status_publisher_->publish(this->slip_status_msg_);
+  });
+
+  kidnap_status_timer_ = create_wall_timer(std::chrono::duration<double>(1 / kidnap_status_publish_rate), [this]() {
+    // Set header timestamp.
+    this->kidnap_status_msg_.header.stamp = now();
+
+    // Set kidnap status.
+    this->kidnap_status_msg_.is_kidnapped = kidnap_status_;
+
+    // Publish topics
+    this->kidnap_status_publisher_->publish(this->kidnap_status_msg_);
+  });
+
+  battery_state_timer_  = create_wall_timer(std::chrono::duration<double>(1 / battery_state_publish_rate), [this]() {
+    // Set header timestamp.
+    this->battery_state_msg_.header.stamp = now();
+
+    this->battery_state_msg_.percentage = 1;
+
+    // Publish topics
+    this->battery_state_publisher_->publish(this->battery_state_msg_);
+  });
+
+  // Set buttons header
+  this->buttons_msg_.header.frame_id = "base_link";
+  this->buttons_msg_.button_1.header.frame_id = "button_1";
+  this->buttons_msg_.button_power.header.frame_id = "button_power";
+  this->buttons_msg_.button_2.header.frame_id = "button_2";
+  // Set buttons pressed state
+  this->buttons_msg_.button_1.is_pressed = false;
+  this->buttons_msg_.button_power.is_pressed = false;
+  this->buttons_msg_.button_2.is_pressed = false;
+
+  // Set slip status header
+  this->slip_status_msg_.header.frame_id = "base_link";
+  // Set slip status status
+  this->slip_status_msg_.is_slipping = false;
+}
+
+void MockPublisher::kidnap_callback(irobot_create_msgs::msg::HazardDetectionVector::SharedPtr msg)
+{
+  std::vector<irobot_create_msgs::msg::HazardDetection> hazard_vector = msg->detections;
+}
+
+void MockPublisher::stop_callback(nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  auto position = msg->pose.pose.position;
+  auto orientation = msg->pose.pose.orientation;
+
+  if(abs(position.x-pose_x)<0.00001 && abs(position.y-pose_y)<0.00001 && abs(position.z-pose_z)<0.00001 &&
+     abs(orientation.x-orientation_x)<0.00001 && abs(orientation.y-orientation_y)<0.00001 &&
+     abs(orientation.z-orientation_z)<0.00001 && abs(orientation.w-orientation_w)<0.00001)
+  {
+    stop_status_ = true;
+  } else {
+    stop_status_ = false;
+  }
+
+  // Store the current position
+  pose_x = position.x;
+  pose_y = position.y;
+  pose_z = position.z;
+
+  orientation_x = orientation.x;
+  orientation_y = orientation.y;
+  orientation_z = orientation.z;
+  orientation_w = orientation.w;
+
+  // Set header timestamp.
+  this->stop_status_msg_.header.stamp = now();
+  this->stop_status_msg_.is_stopped = stop_status_;
+
+  // Publish topics
+  this->stop_status_publisher_->publish(this->stop_status_msg_);
+}
+
+void MockPublisher::lightring_callback(irobot_create_msgs::msg::LightringLeds::SharedPtr msg)
+{
+  auto leds_vector = msg->leds;
+}
+}  // namespace irobot_create_toolbox
